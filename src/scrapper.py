0642c513a3911c6f206a6bef89c205c2bca6a23f
Pedro José Berberana <pedro.jos@gmail.com>
2025-11-07 19:03:27 +0100

    Captura de datos finalizada, el fichero juzgados.json contiene los datos de todos los juzgados y jueces registrados en la web del CGPJ

--- src/scrapper.py
+++ src/scrapper.py
@@ -21,21 +21,25 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 import json
+import re
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 options = Options()
 options.add_argument(f'user-agent={user_agent}')
 options.add_argument("--start-maximized")
 options.add_argument("--disable-blink-features=AutomationControlled") # Oculta "soy un bot"
-RUTADATOS = '../data'
+rutaActual = os.path.dirname(os.path.abspath(__file__))
+RUTADATOS = os.path.join(rutaActual,'../data')
 rutas = {
-  'ficheroProvincias': f'{RUTADATOS}/provincias.json',
-  'ficheroJuzgados': f'{RUTADATOS}/juzgados.json'
+  'ficheroProvincias': os.path.abspath(f'{RUTADATOS}/provincias.json'),
+  'ficheroJuzgados': os.path.abspath(f'{RUTADATOS}/juzgados.json'),
+  'fjcheroFinal': os.path.abspath(f'{RUTADATOS}/jueces-y-juzgados-espanna.csv')
 }
 provinciaClass = {
   'nombre': '',
   'url': '',
-  'juzgados': []
+  'juzgados': [],
+  'numeroJuzgados': 0
 }
 juzgadoClass = {
   'nombre': '',
@@ -47,20 +51,14 @@ juzgadoClass = {
   'detallesObtenidos': False,
   'fax': '',
   'email': '',
+  'latitud': 0.,
+  'longitud': 0.,
   'jueces': []
-}
-
-selectoresDetalleJuzgado = {
-  'coordenadasMapa': 'div.google-maps-link a',
-  'regexLatitudLongitud': r'@(-?\d+\.\d+),(-?\d+\.\d+)',
-  'magistrados': 'div[id=listaTit] table tbody tr td[not(@class)]',
-  'textosDetalle': 'div.txtMapa dl', # Pares dt/dd 
-  'email': 'dd.wrapMail a'
 }
 
 
 CGPJ_ROOTURL = 'https://www.poderjudicial.es'
-SLEEP_TIME = 0.5  # segundos
+SLEEP_TIME = 0.5  # Segundos de espera entre llamadas al navegador
 CGPJ_URLS = {
 	'provincias': f'{CGPJ_ROOTURL}/cgpj/es/Servicios/Directorio/Directorio_de_Organos_Judiciales',
   'juzgados': ''
@@ -140,24 +138,75 @@ def ListarJuzgadosProvincia(provincia):
 def ObtenerDetalleJuzgados(juzgado: dict)-> dict:
   driver.get(juzgado['url'])
   time.sleep(SLEEP_TIME)
-  soup = BeautifulSoup(driver.page_source, 'html.parse')
-  detalles = soup.find_all(selectoresDetalleJuzgado['textosDetalle'])
-  
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  # Capturamos fax y emails del juzgado
+  bloque_detalle = soup.select_one('div#txtMapa')
+  fax = (bloque_detalle
+            .find('dt', string= lambda t: t and 'Fax' in t)
+            .find_next_sibling('dd').get_text(strip=True)         
+          if bloque_detalle.find("dt", string=lambda t: t and "Fax" in t)
+          else None
+        )
+  email = (bloque_detalle
+              .find('dt', string= lambda t: t and 'E-mail' in t)
+              .find_next_sibling('dd').get_text(strip=True)
+            if bloque_detalle.find('dt', string= lambda t: t and 'E-mail' in t)
+            else None
+          )
+  juzgado['fax']=fax
+  juzgado['email']=email
+  # Capturamos los jueces titulares
+  divJueces = soup.find('div', id='listaTit')
+  if divJueces:
+    juzgado['jueces'] = [td.get_text(strip=True) for td in divJueces.select('table td:not([class])') if divJueces.select('table td:not([class])')]
+  # Capturamos latitud y longitud a través de una expresión regular que parsea el contenido del link del mapa Google que se muestra en la página de detalle del Juzgado
+  googleLink = soup.select_one('div#mapaInfo iframe[title="Mapa google"]').get('src') if soup.select_one('div#mapaInfo iframe[title="Mapa google"]') else None
+  if googleLink:
+    grupos = re.search(r'[?&]q=(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?)', googleLink)
+    if grupos and len(grupos.groups())==2:
+      juzgado['latitud'], juzgado['longitud'] = map(float,grupos.groups())
+  juzgado['detallesObtenidos'] = True
   return juzgado
 
 provincias = None
 juzgados = None
-if(os.path.isfile(rutas['ficheroProvincias'])):
-  with open(rutas['ficheroProvincias'], 'r', encoding='utf-8') as f:
-    provincias = json.load(f)
+os.path.abspath(rutas['ficheroProvincias'])
+if(os.path.isfile(rutas['ficheroJuzgados'])):
+  with open(rutas['ficheroJuzgados'], 'r', encoding='utf-8') as jsonFile:
+    try:
+      provincias = json.load(jsonFile)
+    except:
+      provincias = None
+elif(os.path.isfile(rutas['ficheroProvincias'])):
+  with open(rutas['ficheroProvincias'], 'r', encoding='utf-8') as jsonFile:
+    try:
+      provincias = json.load(jsonFile)
+    except:
+      provincias = None
 
 with webdriver.Chrome(options=options) as driver:  
-  if provincias is None:
+  if provincias is None or len(provincias)<=0:
     provincias =ListarProvincias()  
-  juzgadosProvincia = []
-  for provincia in provincias:
+    with open(rutas['ficheroProvincias'], 'w', encoding='utf-8') as jsonFile:
+      json.dump(provincias, jsonFile, indent=2)  
+  iterador = provincias.copy() # Evitamos que nos falle al ir guardando los resultados según capturemos los resultados
+  for provincia in iterador:
+    if not provincia['numeroJuzgados'] is None and provincia['numeroJuzgados']>0: # Si ya hemos capturado los datos, seguimos con la siguiente provincia
+      continue
     print(f'Obteniendo juzgados de {provincia}...')
-    juzgados = ListarJuzgadosProvincia(provincia)
-    with open(rutas['ficheroJuzgados'], 'w', encoding='utf-8'):
-      json.dump(juzgados)
+    juzgadosProvincia = ListarJuzgadosProvincia(provincia)
+    provincia['juzgados'] = juzgadosProvincia
+    provincia['numeroJuzgados'] = len(juzgadosProvincia)
+    with open(rutas['ficheroJuzgados'], 'w', encoding='utf-8') as jsonFile:
+      json.dump(provincias, jsonFile, indent=2)
+  totalJuzgados = sum(x['numeroJuzgados'] for x in provincias)
+  procesados = 0
+  for provincia in provincias:
+    for juzgado in provincia['juzgados']:
+      if(not juzgado['detallesObtenidos']):
+        ObtenerDetalleJuzgados(juzgado)
+      procesados += 1      
+      print(f'Queda obtener el detalle de [{totalJuzgados-procesados} ({100 - (100*procesados/totalJuzgados)} %) ] juzgados')
+  with open(rutas['ficheroJuzgados'], 'w', encoding='utf-8') as jsonFile:
+      json.dump(provincias, jsonFile, indent=2)
 
